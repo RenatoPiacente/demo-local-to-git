8a34733b071b18731e3b37c0c8043a369d2d3d30
Renato Piacente <97764409+RenatoPiacente@users.noreply.github.com>
2022-06-17 10:10:21 -0400

    Updated test.py

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
-name = input('Type your name: ').strip().title()
-age = int(input('Inform your age: '))
+def under_age(age: int):
+    while True:
+        try:
+            user_age = int(input(age))
+            if user_age < 18:
+                print(f'You are {user_age} years old and are a minor')
+            break
+        except(TypeError, KeyError, ValueError):
+            print('Error... Data not accepted. Please inform a number')
+
 
 if __name__ == '__main__':
-    print(f'Hello, {name}. You are {age} years old!')
+    name = input('Type your name: ').strip().title()
+    under_age(age = 'Inform your age: ')
